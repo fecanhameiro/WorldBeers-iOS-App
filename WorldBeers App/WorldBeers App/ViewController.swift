acac83a0e4fdd7a063c03ed6308bb0893312ec4b
Feliipe Canhameiro <felipe.canhameiro@performait.com>
2023-02-18 18:00:20 +0100

    uploading the configured project to run the app with ios 12

---
+++ WorldBeers App/WorldBeers App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldBeers App
+//
+//  Created by Felipe C Canhameiro on 18/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
